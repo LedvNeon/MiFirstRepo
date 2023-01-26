d39b6188cafa48b5c1f31fc4bdc3810ff44f5a3d
DimaKadich <67148653+LedvNeon@users.noreply.github.com>
2023-01-26 17:50:24 +0300

    test.sh - скрипт
    поместить в ту же директорию, что и Vagrantfile

---
+++ test.sh
@@ -0,0 +1,25 @@
+ 
+#!/bin/bash
+          yum install -y http://download.zfsonlinux.org/epel/zfs-release.el7_8.noarch.rpm
+          #import gpg key 
+          rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
+          #install DKMS style packages for correct work ZFS
+          yum install -y epel-release kernel-devel zfs
+          #change ZFS repo
+          yum-config-manager --disable zfs
+          yum-config-manager --enable zfs-kmod
+          yum install -y zfs
+          #Add kernel module zfs
+          modprobe zfs
+          #install wget
+          yum install -y wget
+		  #Создаём пул из двух дисков в режиме RAID 1
+		  zpool create otus1 mirror /dev/sdb /dev/sdc
+		  zpool create otus2 mirror /dev/sdd /dev/sde
+		  zpool create otus3 mirror /dev/sdf /dev/sdg
+		  zpool create otus4 mirror /dev/sdh /dev/sdi
+		  #Добавим разные алгоритмы сжатия в каждую файловую систему:
+		  zfs set compression=lzjb otus1
+		  zfs set compression=lz4 otus2
+		  zfs set compression=gzip-9 otus3
+		  zfs set compression=zle otus4
